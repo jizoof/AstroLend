eb06c347c33401288ef3e1dcd7bd601f89121f59
jizoof <alen29jp@gmail.com>
2026-04-28 19:40:19 +0530

    fix: override Next error page to prevent SSR crash

---
+++ frontend/pages/error.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function ErrorPage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Something went wrong</h1>
+      <p>Please try again later.</p>
+    </div>
+  );
+}
